afcc256c5debbb6bc7c7b86d91806f06bfdc9ac0
Andrew Garner <awgarner@gmail.com>
2022-03-20 10:06:53 -0400

    added variables for password options, possible characters, guaranteed characters, possible characters. started the logic section to concatenate user selected char types to the array of possible characters.  utilized .push to grab a random character from each array and append it to the list of guaranteed characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,78 @@
-// Assignment code here
+// Function to generate password with user input
+function generatePassword() {
+  var options = getPasswordOptions();
+  // variable to store password as it's being concatenated
+  var result = [];
+
+  // Array to store types of characters to include in password
+  var possibleCharacters = [];
+
+  //Array to contain one of each type of chosen character to ensure each will be used
+  var guaranteedCharacters = [];
+
+  // check if options object exists, if not exit the function
+  if (!options) return null;
+
+  // conditional statement that add array of special characters into array of possible characters based on user input
+  // push new random specialcharacter to guaranteedCharacters
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  // conditional statement that adds array of numeric characters into array of possible character based on user input
+  // Push new random special character to guaranteedCharacters
+  if (options.hasNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+
+  }
+
+  // conditional statement that adds array of lowercase characters into array of possible characters based on user input
+  // Push new random lower-cased character to guaranteedCharacters
+  if (options.hasLowerCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+  }
+
+  // conditional statement that adds array of uppercase characters into array of possible characters based on user input
+
+  if (options.hasUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Get references to the #generate element
