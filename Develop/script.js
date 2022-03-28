becf57f61b6bc73680e6b6279d62cb9c8acd8823
Andrew Garner <awgarner@gmail.com>
2022-03-28 11:56:52 -0400

    added lowercase array and added appscreenshot.jpg

--- Develop/script.js
+++ Develop/script.js
@@ -176,8 +176,31 @@ function generatePassword() {
     ]
     const lowerCasedCharacters = [
       "a",
-      "b"
-
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z"
     ]
 
 
